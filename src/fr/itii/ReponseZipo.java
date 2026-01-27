2a8fc1ac366f204a0d1ec11a706fecfef5bcc599
ju-netsys <ju.roquigny@gmail.com>
2026-01-27 09:11:59 +0100

    1st version

---
+++ src/fr/itii/ReponseZipo.java
@@ -0,0 +1,17 @@
+package fr.itii;
+
+import java.util.List;
+
+public class ReponseZipo {
+
+    private String country;
+    private List<fr.itii.lieu_objet> places;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public List<fr.itii.lieu_objet> getPlaces() {
+        return places;
+    }
+}
---
+++ src/fr/itii/lieu_objet.java
@@ -0,0 +1,28 @@
+package fr.itii;
+
+import com.google.gson.annotations.SerializedName;
+
+public class lieu_objet {
+
+    @SerializedName("place name")
+    private String nom;
+    private String state;
+    private String latitude;
+    private String longitude;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+}
